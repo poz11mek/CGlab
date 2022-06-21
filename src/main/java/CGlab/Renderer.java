39f6a0dc78869b4961ff89e360233d9e5e29ddfd
Patryk Ozimek <ozimekpatryk10@gmail.com>
2022-06-21 20:51:00 +0200

    zad.4 i 5

--- src/main/java/CGlab/Renderer.java
+++ src/main/java/CGlab/Renderer.java
@@ -79,11 +79,74 @@ public class Renderer {
                 y += (y1 > y0 ? 1 : -1);
                 err -= 1.;
             }
-        } // Oktanty: 8
+        }
     }
 
     public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
-        // TODO: zaimplementuj
+        if(Math.abs(y1-y0) < Math.abs(x1-x0))
+            if(x0>x1)
+                plotLineLow(x1, y1, x0, y0);
+            else
+                plotLineLow(x0, y0, x1, y1);
+        else
+        if(y0>y1)
+            plotLineHigh(x1, y1, x0, y0);
+        else
+            plotLineHigh(x0, y0, x1, y1);
+    }
+
+    private void plotLineLow(int x0, int y0, int x1, int y1) {
+        int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
+
+        int yi, err, x, y;
+        int dx = x1-x0;
+        int dy = y1-y0;
+
+        yi = 1;
+        if(dy<0) {
+            yi = -1;
+            dy = -dy;
+        }
+
+        err = (2*dy) - dx;
+        y = y0;
+
+        for (x = x0; x < x1; x++) {
+            render.setRGB(x, y, white);
+            if(err>0) {
+                y += yi;
+                err = err + (2*(dy-dx));
+            } else {
+                err = err + 2*dy;
+            }
+        }
+    }
+
+    private void plotLineHigh(int x0, int y0, int x1, int y1) {
+        int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
+
+        int xi, err, x, y;
+        int dx = x1-x0;
+        int dy = y1-y0;
+
+        xi = 1;
+        if(dx<0) {
+            xi = -1;
+            dx = -dx;
+        }
+
+        err = (2*dx) - dy;
+        x = x0;
+
+        for (y = y0; y < y1; y++) {
+            render.setRGB(x, y, white);
+            if(err>0) {
+                x += xi;
+                err = err + (2*(dx-dy));
+            } else {
+                err = err + 2*dx;
+            }
+        }
     }
 
     public void save() throws IOException {
